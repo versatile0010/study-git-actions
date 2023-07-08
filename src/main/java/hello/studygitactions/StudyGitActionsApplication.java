95b9717674d3b38e8079af96db4417af7ab700ec
versatile0010 <siam0294@naver.com>
2023-07-09 00:11:57 +0900

    init

---
+++ src/main/java/hello/studygitactions/StudyGitActionsApplication.java
@@ -0,0 +1,13 @@
+package hello.studygitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGitActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGitActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/studygitactions/StudyGitActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.studygitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGitActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
