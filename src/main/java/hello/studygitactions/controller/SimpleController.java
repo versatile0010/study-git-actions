adff1cc0214936125f205e9dcaa126966601505e
versatile0010 <siam0294@naver.com>
2023-07-09 00:58:42 +0900

    init

--- src/main/java/hello/studygitactions/controller/SimpleController.java
+++ src/main/java/hello/studygitactions/controller/SimpleController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleController {
     @GetMapping("/first")
     public String helloWorld(){
-        return "Hello world!!!";
+        return "Hello world!!!!";
     }
 }
